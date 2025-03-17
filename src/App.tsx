de72acb59b05a0d1036c65a2831edc82546efb85
williamfarmer <william.miles.farmer@gmail.com>
2025-03-16 23:20:33 -0700

    Works in vite and react, got the aliases and path names and some typescript types cleaned up. I think it's ready for the big refactor from what the alpha version did. I'm not really concerned about local dev being asynch with no real progress monitoring as long as it completes for now. Asynch promise bucket catching can be done later once I get the actual steps cleaned up and working.

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ export const App = () => {
   console.log('App loaded!'); // Debug here
   return (
     <div style={{ padding: '1rem' }}>
-      <h1>APL Doom React</h1>
+      <h1>Doom React/Vite/WebGL</h1>
       <DoomCanvas />
     </div>
   );
