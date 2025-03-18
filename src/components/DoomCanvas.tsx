83b7b3670344c338c1f76e9b3eb6800809072e01
williamfarmer <william.miles.farmer@gmail.com>
2025-03-17 23:58:57 -0700

    Moved frag and vert files out

--- src/components/DoomCanvas.tsx
+++ src/components/DoomCanvas.tsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useRef, useState } from 'react';
-import { renderGame } from '@/parser/render/renderGame';
+import { renderGame } from '@/wad/renderer/renderGame';
 import { useDoomLoader } from '@/hooks/useDoomLoader';
 
 export const DoomCanvas: React.FC = () => {
